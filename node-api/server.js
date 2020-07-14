d847a7693ca9e338b48f93615905018144059f44
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-14 19:17:49 -0300

    api completa e rodando no insonmia

--- node-api/server.js
+++ node-api/server.js
@@ -1,20 +1,27 @@
 const express = require('express');
-
+const mongoose = require('mongoose')
+const requireDIR = require('require-dir')
 const app = express();
+const cors = require('cors')
+
+capp.use(cors())
+app.use(express.json())
+
+//iniciando banco de dados
+mongoose.connect(
+  'mongodb://MarjSantos:3655@huntweb-shard-00-00.5nxju.mongodb.net:27017,huntweb-shard-00-01.5nxju.mongodb.net:27017,huntweb-shard-00-02.5nxju.mongodb.net:27017/HuntWeb?ssl=true&replicaSet=atlas-561s7s-shard-0&authSource=admin&retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Mongodb conectado com sucesso')
+
+  }).catch((err) => {
+    console.log(err)
+  });
 
 
-//quando o user acessar a rota vamos mostrar uma mensagem na tela
-// / = rota raiz
-// primeiro parâmetro = o que vai ser exibido, segundo parâmetro = função
-// req => a requisição feita para o servidor
-// res => resposta para a requisição/usuario
+requireDIR('./src/models');
+
+// const product = mongoose.model('product')
 
 
-app.get('/', (re, res) => {
-  res.send('Hello, sou a Marjorie, estou testando')
-});
-
+app.use('/api', require('./src/routs'))
 app.listen(3001);
-
-// IRÁ ATUALIZAR AUTOMATICAMENTE O SERVER QUANDO ALGUMA ALTERAÇÃO FOR FEITA
-// NPM INSTALL -D NODEMON
